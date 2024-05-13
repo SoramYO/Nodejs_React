3b02ac7f120be95cc9c63cf10a757a90216ca7db
SoramYO <ngoxuanson121@gmail.com>
2024-05-13 17:33:31 +0700

    Kết Nối Tới MySQL Database với Sequelize

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,21 @@
+import db from '../models/index.js';
 
+let getHomePage = async (req, res) => {
+    try {
 
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+        let data = await db.User.findAll();
+        console.log('-------------------------------')
+        console.log(data);
+        console.log('-------------------------------')
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+    }
+
 }
 
 module.exports = {
